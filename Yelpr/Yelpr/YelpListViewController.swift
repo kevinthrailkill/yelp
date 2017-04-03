eeabdcf36d0df03343a2c01b136b646e5a45fc2a
Kevin Thrailkill <kevinjt92@gmail.com>
2017-04-03 15:02:42 -0700

    Inifite Scroll Refactor

--- Yelpr/Yelpr/YelpListViewController.swift
+++ Yelpr/Yelpr/YelpListViewController.swift
@@ -88,9 +88,9 @@ class YelpListViewController: UIViewController {
                 self.businessList = businesses
                 self.businessListTableView.reloadData()
                 
-                if businesses.count > 0 {
-                    self.businessListTableView.scrollToRow(at: IndexPath(row: 0, section: 0), at: UITableViewScrollPosition.top, animated: true)
-                }
+              //  if businesses.count > 0 {
+               //     self.businessListTableView.scrollToRow(at: IndexPath(row: 0, section: 0), at: UITableViewScrollPosition.top, animated: true)
+              //  }
                 
             }else{
                 //error
@@ -214,40 +214,6 @@ extension YelpListViewController : UIScrollViewDelegate {
     
 }
 
-class InfiniteScrollActivityView: UIView {
-    var activityIndicatorView: UIActivityIndicatorView = UIActivityIndicatorView()
-    static let defaultHeight:CGFloat = 60.0
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        setupActivityIndicator()
-    }
-    
-    override init(frame aRect: CGRect) {
-        super.init(frame: aRect)
-        setupActivityIndicator()
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        activityIndicatorView.center = CGPoint(x: self.bounds.size.width/2, y: self.bounds.size.height/2)
-    }
-    
-    func setupActivityIndicator() {
-        activityIndicatorView.activityIndicatorViewStyle = .gray
-        activityIndicatorView.hidesWhenStopped = true
-        self.addSubview(activityIndicatorView)
-    }
-    
-    func stopAnimating() {
-        self.activityIndicatorView.stopAnimating()
-        self.isHidden = true
-    }
-    
-    func startAnimating() {
-        self.isHidden = false
-        self.activityIndicatorView.startAnimating()
-    }
-}
+
 
 
