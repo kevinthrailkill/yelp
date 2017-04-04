1449e6f2959a05ab3136c11ac5ab12e29747ce3c
Kevin Thrailkill <kevinjt92@gmail.com>
2017-04-03 17:36:49 -0700

    New Filters page setup

---
+++ Yelpr/Yelpr/CategoryCell.swift
@@ -0,0 +1,40 @@
+//
+//  CategoryCell.swift
+//  Yelpr
+//
+//  Created by Kevin Thrailkill on 4/3/17.
+//  Copyright © 2017 kevinthrailkill. All rights reserved.
+//
+
+import UIKit
+
+
+protocol CategoryCellDelegate: class {
+    func category(cell: CategoryCell, didChangeValue value: Bool)
+}
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var categorySwitch: UISwitch!
+    
+    
+    weak var delegate: CategoryCellDelegate?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+    @IBAction func catgeoryChanged(_ sender: Any) {
+        delegate?.category(cell: self, didChangeValue: categorySwitch.isOn)
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Yelpr/Yelpr/FilterPageViewController.swift
+++ Yelpr/Yelpr/FilterPageViewController.swift
@@ -20,27 +20,10 @@ class FilterPageViewController: UITableViewController {
     
     var filterPreferences : FilterPreferences?
     weak var filterDelegate: YelpFilterDelegate?
+    var selectedDistanceRow : Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        let checkedDistanceCell = tableView.cellForRow(at: IndexPath(row: (filterPreferences?.distanceAway.rawValue)!, section: 1))
-        
-        checkedDistanceCell?.accessoryType = UITableViewCellAccessoryType.checkmark
-        
-        let checkedSortByCell = tableView.cellForRow(at: IndexPath(row: (filterPreferences?.sortValue.rawValue)!, section: 2))
-        
-        checkedSortByCell?.accessoryType = UITableViewCellAccessoryType.checkmark
-        
-        
-    }
-    
-    @IBAction func hasDealChanged(_ sender: UISwitch) {
-        filterPreferences?.hasDeal = sender.isOn
     }
     
 
@@ -51,35 +34,146 @@ class FilterPageViewController: UITableViewController {
 
     // MARK: - Table view data source
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 4
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if section == 0 {
+            return 1
+        }else if section == 1 {
+            return 5
+        }else if section == 2 {
+            return 3
+        }else if section == 3 {
+            return categories.count
+        }
+        
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        if section == 0 {
+            return nil
+        }else if section == 1 {
+            return "Distance"
+        }else if section == 2 {
+            return "Sort By"
+        } else if section == 3 {
+            return "Categories"
+        }
+        
+        return nil
+    }
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath?{
+        if indexPath.section == 0 || indexPath.section == 3 {
+            return nil
+        }
+        
+        return indexPath
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let section = indexPath.section
+        
+        if section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "DealsCell", for: indexPath) as! DealsCell
+            cell.dealSwitch.isOn = filterPreferences!.hasDeal
+            cell.delegate = self
+            
+            
+            return cell
+
+        }else if section == 1 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "DistanceCell", for: indexPath) as! DistanceCell
+            
+            let disVal = YelpDistanceAway(rawValue: indexPath.row)
+            
+            cell.distanceLabel.text = disVal!.description
+            if disVal == filterPreferences!.distanceAway {
+                cell.accessoryType = .checkmark
+            }else{
+                cell.accessoryType = .none
+            }
+            
+            return cell
+
+            
+        }else if section == 2 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "SortCell", for: indexPath) as! SortCell
+            
+            
+            let sortVal = YelpSortDescriptor(rawValue: indexPath.row)
+            
+            cell.sortLabel.text = sortVal!.description
+            if sortVal == filterPreferences!.sortValue {
+                cell.accessoryType = .checkmark
+            }else{
+                cell.accessoryType = .none
+            }
+            
+            return cell
+        } else if section == 3 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath) as! CategoryCell
+            
+            
+            cell.categoryLabel.text = categories[indexPath.row]["name"]
+            cell.delegate = self
+            
+            return cell
+        }
+        
+        
+
+        return UITableViewCell()
+        
+    }
+    
+    
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
         let section = indexPath.section
+        
+        
         let numberOfRows = tableView.numberOfRows(inSection: section)
         for row in 0..<numberOfRows {
             if let cell = tableView.cellForRow(at: IndexPath(row: row, section: section)) {
                 cell.accessoryType = row == indexPath.row ? .checkmark : .none
-                //
+                
             }
         }
         
         if section == 1 {
-            filterPreferences?.distanceAway = YelpDistanceAway(rawValue: indexPath.row)!
+            filterPreferences!.distanceAway = YelpDistanceAway(rawValue: indexPath.row)!
+            
         }
         
         if section == 2 {
-            filterPreferences?.sortValue = YelpSortDescriptor(rawValue: indexPath.row)!
+            filterPreferences!.sortValue = YelpSortDescriptor(rawValue: indexPath.row)!
         }
         
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+
+extension FilterPageViewController : DealsCellDelegate, CategoryCellDelegate {
+    func dealSwitchChanged(val: Bool) {
+        filterPreferences!.hasDeal = val
+        print(filterPreferences!.hasDeal)
+    }
+    
+    func category(cell: CategoryCell, didChangeValue value: Bool) {
+        print(cell.categoryLabel.text)
+        print(value)
+    }
+    
+    
+}
+
+
